986c14286ff65f7874dc1704a6c47fb47f3948d5
Joost Schouwstra <63044232+jschouw1@users.noreply.github.com>
2020-08-17 12:57:03 +0200

    Initial versions RestControllers

--- src/main/java/com/jts/huiswerkTennet/controller/ContactPersonenController.java
+++ src/main/java/com/jts/huiswerkTennet/controller/ContactPersonenController.java
@@ -1,4 +1,20 @@
 package com.jts.huiswerkTennet.controller;
 
+import com.jts.huiswerkTennet.model.ContactPersonen;
+import com.jts.huiswerkTennet.service.ContactPersonenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContactPersonenController {
+
+    @Autowired private ContactPersonenService contactPersonenService;
+
+    @PostMapping("/contactPersonen")
+    public ContactPersonen create(@RequestBody ContactPersonen contactPersonen){
+        return contactPersonenService.save(contactPersonen);
+    }
+
 }
--- src/main/java/com/jts/huiswerkTennet/controller/ProductieInstallatiesController.java
+++ src/main/java/com/jts/huiswerkTennet/controller/ProductieInstallatiesController.java
@@ -1,4 +1,19 @@
 package com.jts.huiswerkTennet.controller;
 
+import com.jts.huiswerkTennet.model.ProductieInstallaties;
+import com.jts.huiswerkTennet.service.ProductieInstallatiesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductieInstallatiesController {
+
+    @Autowired private ProductieInstallatiesService productieInstallatiesService;
+
+    @PostMapping("/productieInstallaties")
+    public ProductieInstallaties create(@RequestBody ProductieInstallaties productieInstallaties){
+        return productieInstallatiesService.save(productieInstallaties);
+    }
 }
