29e7fe6b2d995be56ef38266d69fd33bcb0d664a
Joost Schouwstra <63044232+jschouw1@users.noreply.github.com>
2020-08-17 12:41:30 +0200

    Filled model classes

--- src/main/java/com/jts/huiswerkTennet/model/ContactPersonen.java
+++ src/main/java/com/jts/huiswerkTennet/model/ContactPersonen.java
@@ -1,4 +1,70 @@
 package com.jts.huiswerkTennet.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ContactPersonen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int contactPersonenId;
+    String name;
+    String zipCode;
+    String city;
+    String number;
+
+    public ContactPersonen() {
+    }
+
+    public ContactPersonen(String name, String zipCode, String city, String number) {
+        this.name = name;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactPersonen{" +
+                "name='" + name + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", city='" + city + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/jts/huiswerkTennet/model/ProductieInstallaties.java
+++ src/main/java/com/jts/huiswerkTennet/model/ProductieInstallaties.java
@@ -1,4 +1,59 @@
 package com.jts.huiswerkTennet.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ProductieInstallaties {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int productieInstallatiesId;
+    String name;
+    int contact;
+    double outputPower;
+
+    public ProductieInstallaties() {
+    }
+
+    public ProductieInstallaties(String name, int contact, double outputPower) {
+        this.name = name;
+        this.contact = contact;
+        this.outputPower = outputPower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public double getOutputPower() {
+        return outputPower;
+    }
+
+    public void setOutputPower(double outputPower) {
+        this.outputPower = outputPower;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductieInstallaties{" +
+                "name='" + name + '\'' +
+                ", contact=" + contact +
+                ", outputPower=" + outputPower +
+                '}';
+    }
 }
