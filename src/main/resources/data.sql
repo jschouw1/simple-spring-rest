0c14bd2b31d67ce1c3662dff938786cdcae40ae3
Joost Schouwstra <63044232+jschouw1@users.noreply.github.com>
2020-08-18 11:40:24 +0200

    Changed initial dB input

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,11 +17,15 @@ CREATE TABLE productie_installaties (
 );
 
 INSERT INTO contact_personen (name, zip_code, city, number) VALUES
-    ('My installation', '0000AA', 'Arnhem', '12a');
+    ('My installation1', '6547RR', 'Arnhem', '12a'),
+    ('My installation2', '8456TT', 'Arnhem', '12a'),
+    ('My installation3', '9764RL', 'Arnhem', '12a'),
+    ('My installation4', '1802JT', 'Arnhem', '12a'),
+    ('My installation5', '2045NN', 'Arnhem', '12a');
 
 INSERT INTO productie_installaties (name, contact, output_power) VALUES
-    ('My installation Laagst', '123', '0.0001'),
-    ('My installation Lager', '456', '245.134896'),
-    ('My installation Gemiddeld', '789', '5000.984'),
-    ('My installation Hoger', '654', '7896.2547'),
-    ('My installation Hoogst', '852', '999999');
+    ('Lowest OP', '123', '0.0001'),
+    ('Low OP', '456', '245.134896'),
+    ('Average OP', '789', '5000.984'),
+    ('High OP', '654', '7896.2547'),
+    ('Highest OP', '852', '999999');
