b6b2f39a18c6eac65a849a3b861e85b138936436
Joost Schouwstra <63044232+jschouw1@users.noreply.github.com>
2020-08-18 12:00:00 +0200

    Changed initial dB input

--- src/main/java/com/jts/huiswerkTennet/HuiswerkTennetApplication.java
+++ src/main/java/com/jts/huiswerkTennet/HuiswerkTennetApplication.java
@@ -1,14 +1,19 @@
 package com.jts.huiswerkTennet;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HuiswerkTennetApplication {
+public class HuiswerkTennetApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HuiswerkTennetApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
 
